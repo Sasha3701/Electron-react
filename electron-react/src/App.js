dd1ffd8dde084a14409ae1f6db05b91e6128ab60
Alex Irhin <irhin_a@garpix.com>
2021-08-24 22:16:27 +0300

    Added layout

--- electron-react/src/App.js
+++ electron-react/src/App.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+import { KEY, BASE_URL } from "./const"
+import axios from "axios"
+
 const App = () => {
+
+  const [rates, setRates] = useState([])
+  const [date, setDate] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/latest?access_key=${KEY}`).then(res => {
+      const { rates, date } = res.data
+      setRates(rates)
+      setDate(date)
+    })
+  })
+
   return (
     <div>
       <h1>Hello Electron React App!!!</h1>
---
+++ electron-react/src/const.js
@@ -0,0 +1,3 @@
+export const KEY = "a66e7d2e57cc9947734ffb192eecbee5"
+
+export const BASE_URL = "http://data.fixer.io/api"
